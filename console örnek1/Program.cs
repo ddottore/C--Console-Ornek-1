2f3accbefa13785dd308d271147e0726805e2ad1
Emirhan Çalışkan <emrhncalskn@gmail.com>
2022-02-16 22:03:44 +0300

    Her sene yapılan zamma göre kaç sene sonra ne kadar maaş alınacağının hesabı

---
+++ console örnek1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Maaşın ne kadar? : ");
+int cevap1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Kaç sene sonraki maaşını öğrenmek istiyorsun? : ");
+int cevap2 = Convert.ToInt32(Console.ReadLine());
+
+if (cevap2 == 0)
+{
+    Console.WriteLine(cevap1);
+}
+
+else
+{
+    int maas = ((cevap1 * 15 / 100) + cevap1) * cevap2;
+
+    Console.WriteLine(maas);
+}
+
+
+
+//Her sene yapılan zamma göre kaç sene sonra ne kadar maaş alınacağının hesabı
